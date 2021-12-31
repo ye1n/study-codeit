0e30098632176a924d78ac07d78a65f22435a46f
ye1n <uyeinyein@gmail.com>
2021-12-31 22:35:21 +0900

    프로그래밍 기초 in JavaScript 4일차

--- programmingBasicsInJavaScript/Topic3.js
+++ programmingBasicsInJavaScript/Topic3.js
@@ -5,13 +5,13 @@ console.log('___________객체___________');
 
 let codeit = {
     // propertyName : property value
-    name : '코드잇',
-    'born Year' : 2017,
-    isVeryNice : true,
-    worstCourse : null,
-    bestCourse : {
-        title : '자바스크립트 프로그래밍 기초',
-        language : 'JavaScript'
+    name: '코드잇',
+    'born Year': 2017,
+    isVeryNice: true,
+    worstCourse: null,
+    bestCourse: {
+        title: '자바스크립트 프로그래밍 기초',
+        language: 'JavaScript'
     }
 };
 
@@ -37,13 +37,13 @@ codeit.name = 'codeit'; // 프로퍼티 변경
 console.log(codeit.name);
 
 console.log(codeit.ceo);
-codeit.ceo = '강영훈'// 프로퍼티 추가
+codeit.ceo = '강영훈' // 프로퍼티 추가
 console.log(codeit.ceo);
 
 delete codeit.worstCourse; // 프로퍼티 지우기
 
 // 'propertyName' in object
-console.log('name' in codeit); 
+console.log('name' in codeit);
 
 // 메소드 (Method) : 연관성 있는 여러 함수를 하나로 묶을 때 사용
 let greetings = {
@@ -64,8 +64,8 @@ greetings['sayHello']('Codeit');
 
 // 직사각형
 let rectAngle = {
-    width : 30,
-    height : 50,
+    width: 30,
+    height: 50,
     getArea: function() {
         return rectAngle.width * rectAngle.height;
     }
@@ -73,8 +73,8 @@ let rectAngle = {
 
 // 삼각형
 let triAngle = {
-    width : 15,
-    height : 40,
+    width: 15,
+    height: 40,
     getArea: function() {
         return rectAngle.width * rectAngle.height / 2;
     }
@@ -97,9 +97,9 @@ let myObject = {
     1: '정수1',
     birthDay: '2017.5.17',
     2: '정수2',
-  };
-  
-  for (let key in myObject) {
+};
+
+for (let key in myObject) {
     console.log(key);
 }
 
@@ -220,10 +220,87 @@ console.log(courseRanking); // ['마지막 강의', '마지막 강의', '미완
 let influencer = ['suwonlog', 'small.tiger', 'Minam.ludens', 'cu_convenience24'];
 
 // for .. in 문은 배열에 사용 권장 X (객체에 적합하게 설계된 반복문)
-for(let element of influencer) {
+for (let element of influencer) {
     console.log(element);
 }
 
 // 다차원 배열
-let twoKimensional = [[1, 2], [3, 4]];
+let twoKimensional = [
+    [1, 2],
+    [3, 4]
+];
 console.log(twoKimensional[0][1]); // 2
+
+console.log('_________자료형 심화_________');
+
+// 숫자 표기법
+
+let millionaire = 1000000000;
+let myNumber1 = 1e9;
+// 지수 표기법 : e 왼편에 있는 수에 오른쪽에 있는 수만큼 10의 거듭제곱을 곱하는 의미
+
+console.log(millionaire == myNumber1);
+
+console.log(25e5 == 2500000);
+console.log(5.3e3 == 5300);
+console.log(-6.1e8 == -610000000);
+
+console.log(16e-5 == 0.00016); // -가 들어가면 알파벳 오른쪽 숫자만큼 10의 거듭제곱으로 나누라는 의미
+console.log(3.5e-3 == 0.0035);
+console.log(-9.1e-5 == -0.000091);
+
+// 16진법
+let hex1 = 0xff;
+let hex2 = 0xFF;
+
+// 8진법
+let octal = 0o377;
+
+// 2진법
+let binary = 0b11111111;
+
+console.log(hex1);
+
+// 숫자형 메소드
+let myNumber2 = 3.3591;
+
+// toFixed (0 ~ 100) : 소수점 아래 자릿수 고정 (부족한 자릿수는 0)
+console.log(myNumber2.toFixed(7)); // typeof string
+console.log(+myNumber2.toFixed(7)); // typeof number
+
+// toString(2 ~ 36)
+console.log(myNumber2.toString(2));
+console.log(+myNumber2.toString(8));
+
+// console.log(255.toString(2)); -> 소수점으로 인식
+console.log(255..toString(2));
+console.log((255).toString(8));
+
+// Math 객체
+
+// Math.abs : 절댓값
+console.log(Math.abs(-10));
+
+// Math.max : 최댓값
+console.log(Math.max(2, -1, 4, 5, 0));
+
+// Math.min : 최솟값
+console.log(Math.min(2, -1, 4, 5, 0));
+
+// Math.pow : 거듭제곱
+console.log(Math.pow(2, 3));
+
+// Math.sqrt : 제곱근
+console.log(Math.sqrt(25));
+
+// Math.round : 반올림
+console.log(Math.round(2.3));
+
+// Math.floor : 버림
+console.log(Math.floor(2.4));
+
+// Math.ceil : 올림
+console.log(Math.ceil(2.4));
+
+// Math.random : 난수 (0~1 사이 값 랜덤 리턴)
+console.log(Math.round(Math.random()));
